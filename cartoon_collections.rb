065cb7f9408a8a6e9fee993cee6747b4eeee2be8
Catherine Price <catprice444@gmail.com>
2020-04-29 21:11:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,13 +6,13 @@ def roll_call_dwarves(array)
   end
 end
 
-def roll_call_dwarves(array)
-  i = 0
-  while i < array.length
-    puts "#{i + 1}. #{array[i]}"
-    i += 1
-  end
-end
+# def roll_call_dwarves(array)
+#   i = 0
+#   while i < array.length
+#     puts "#{i + 1}. #{array[i]}"
+#     i += 1
+#   end
+# end
 
 def summon_captain_planet# code an argument here
   # Your code here
